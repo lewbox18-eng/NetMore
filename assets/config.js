bfa43591585345a8efb91f2a54a85f36dcce67f4
lewbox18-eng <lewbox18@gmail.com>
2026-04-26 11:18:45 +0100

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,6 @@
 window.GitShopConfig = Object.freeze({
   shopName: "NetMore",
   apiBaseUrl: "https://netmore.up.railway.app",
-  shopUrl: "https://lewbox18-eng.github.io/NetMoreSMP",
+  shopUrl: "https://lewbox18-eng.github.io/NetMore",
   currency: "USD"
 });
